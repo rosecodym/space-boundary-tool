20fdcc4b3076f60cf686853253986866cf6a4fc3
cmrose <cmrose@lbl.gov>
2012-07-12 15:50:16 -0700

    removed a bunch of dead code

--- Core/src/operations.h
+++ Core/src/operations.h
@@ -2,22 +2,10 @@
 
 #include "precompiled.h"
 
-#include "element.h"
-#include "guid_filter.h"
-#include "space.h"
-
-struct element_info;
-struct space_info;
-struct space_boundary;
 class surface;
-class equality_context;
 
 namespace operations {
 
-std::vector<std::shared_ptr<surface>>	build_blocks(const std::vector<std::shared_ptr<element>> & elements, std::shared_ptr<equality_context> context);
-std::vector<std::shared_ptr<surface>>	build_stacks(const std::vector<std::shared_ptr<surface>> & blocked_surfaces, const std::vector<std::shared_ptr<space>> & spaces, std::shared_ptr<equality_context> & c);
-std::vector<std::shared_ptr<surface>>	assign_openings(const std::vector<std::shared_ptr<surface>> & surfaces);
-std::vector<std::shared_ptr<surface>>	attach_to_spaces(const std::vector<std::shared_ptr<surface>> & surfaces, const std::vector<std::shared_ptr<space>> & spaces);
-std::vector<std::shared_ptr<surface>>	resolve_levels(const std::vector<std::shared_ptr<surface>> & assigned_surfaces);
+std::vector<std::shared_ptr<surface>> assign_openings(const std::vector<std::shared_ptr<surface>> & surfaces);
 
 }
