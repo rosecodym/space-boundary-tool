5e5df315ba19a80f06d42f49850080782cc1a803
cmrose <cmrose@lbl.gov>
2012-07-25 16:01:04 -0700

    added LoopCleanup.DirectionChangePoint2 and LoopCLeanup.ComplexTwoPassesPoint2

--- Tests/src/loop_cleanup_tests.cpp
+++ Tests/src/loop_cleanup_tests.cpp
@@ -47,4 +47,37 @@ TEST(LoopCleanup, ThreePointDuplicateEndPoint3) {
 	EXPECT_FALSE(geometry_common::cleanup_loop(&vec, g_opts.equality_tolerance));
 }
 
+TEST(LoopCleanup, DirectionChangePoint2) {
+	point_2 pts[] = {
+		point_2(-9.0, 205.650477),
+		point_2(-9.0, 205.983810),
+		point_2(-9.0, 204.067143),
+		point_2(0.0, 204.067143),
+		point_2(0.0, 205.650477)
+	};
+	std::vector<point_2> vec(pts, pts + 5);
+	ASSERT_TRUE(geometry_common::cleanup_loop(&vec, g_opts.equality_tolerance));
+	EXPECT_EQ(4, vec.size());
+}
+
+// strictly speaking, this test is capturing a bug
+// (specifically, "cleaned" loops coming out of cleanup_loop aren't necessarily clean!)
+TEST(LoopCleanup, ComplexTwoPassesPoint2) {
+	point_2 pts[] = {
+		point_2(-9.000000, 205.650477),
+		point_2(-9.000000, 205.983810),
+		point_2(0.000000, 205.983810),
+		point_2(0.000000, 205.983810),
+		point_2(-9.000000, 205.983810),
+		point_2(-9.000000, 204.067143),
+		point_2(0.000000, 204.067143),
+		point_2(0.000000, 205.650477)
+	};
+	std::vector<point_2> vec(pts, pts + 8);
+	ASSERT_TRUE(geometry_common::cleanup_loop(&vec, g_opts.equality_tolerance));
+	ASSERT_EQ(5, vec.size());
+	ASSERT_TRUE(geometry_common::cleanup_loop(&vec, g_opts.equality_tolerance));
+	EXPECT_EQ(4, vec.size());
+}
+
 } // namespace geometry_2d
