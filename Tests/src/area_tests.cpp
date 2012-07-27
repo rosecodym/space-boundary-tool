8a341779819f34765aed4b2ec716fbaaef575048
cmrose <cmrose@lbl.gov>
2012-07-27 15:01:24 -0700

    added Area.SubtractionIsValidVertexOnDiagonal

--- Tests/src/area_tests.cpp
+++ Tests/src/area_tests.cpp
@@ -3,6 +3,10 @@
 #include <gtest/gtest.h>
 
 #include "area.h"
+#include "equality_context.h"
+#include "sbt-core.h"
+
+extern sb_calculation_options g_opts;
 
 namespace geometry_2d {
 
@@ -44,6 +48,29 @@ TEST(Area, IntersectionInvariability) {
 	EXPECT_EQ(
 		area::do_intersect(larger, smaller),
 		area::do_intersect(larger, smaller));
+}
+
+TEST(Area, SubtractionIsValidVertexOnDiagonal) {
+	equality_context c(g_opts.equality_tolerance);
+	point_2 op1[] = {
+		point_2(27.443970, 8.339190),
+		point_2(28.769285, 8.339190),
+		point_2(28.769285, 8.844385),
+		point_2(27.443970, 8.844385)
+	};
+	point_2 op2[] = {
+		point_2(28.665582, 8.744685),
+		point_2(28.883634, 8.954685),
+		point_2(28.883634, 11.736685),
+		point_2(28.665582, 11.736685)
+	};
+	for (size_t i = 0; i < 4; ++i) {
+		// this is in the test because the contextualization is relevant
+		c.snap(&op1[i]);
+		c.snap(&op2[i]);
+	}
+	area res = area(polygon_2(op1, op1 + 4)) - area(polygon_2(op2, op2 + 4));
+	EXPECT_TRUE(res.is_valid());
 }
 
 } // namespace
