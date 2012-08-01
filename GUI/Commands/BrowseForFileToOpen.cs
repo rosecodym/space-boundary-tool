b548ebfe56f3a84346c2eae70929af8099c76593
cmrose <cmrose@lbl.gov>
2012-08-01 12:12:37 -0700

    removed old gui frontend - added beginnings of new one, with proper mvvm

---
+++ GUI/Commands/BrowseForFileToOpen.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace GUI.Commands
+{
+    class BrowseForFileToOpen : ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            return false;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GUI/ViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+using Microsoft.Win32;
+
+namespace GUI
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        private string _inputIfcPath;
+        private string _outputIfcPath;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand BrowseToInputIfcFileCommand { get; private set; }
+
+        public string InputIfcFilePath
+        {
+            get { return _inputIfcPath; }
+            set
+            {
+                _inputIfcPath = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("InputIfcFilePath"));
+                }
+            }
+        }
+
+        public ViewModel()
+        {
+            BrowseToInputIfcFileCommand = new DelegateCommand((_) => BrowseToInputIfcFile());
+        }
+
+        private void BrowseToInputIfcFile()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            bool? result = ofd.ShowDialog();
+            if (result.HasValue && result.Value == true)
+            {
+                InputIfcFilePath = ofd.FileName;
+            }
+        }
+
+        /*
+         *  
+
+        public ViewModel()
+        {
+            BrowseCommand = new DelegateCommand((o) => BrowseToFile());
+        }
+
+        private void BrowseToFile()
+        {
+            var openFileDialog = new OpenFileDialog();
+            var result = openFileDialog.ShowDialog();
+
+            if (result.HasValue && result.Value == true)
+            {
+                FilePath = openFileDialog.FileName;
+            }
+        }*/
+    }
+}
