9037bc6475c57c26fd5091120cb16db63c28d3e1
cmrose <cmrose@lbl.gov>
2012-08-07 14:58:19 -0700

    binding for entire sb summary

--- GUI/Converters.cs
+++ GUI/Converters.cs
@@ -21,4 +21,104 @@ namespace GUI.Converters
             throw new InvalidOperationException();
         }
     }
+
+    [ValueConversion(typeof(BuildingInformation), typeof(string))]
+    public class BuildingInformationTo2ndLevelPhysicalInternalCountString : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            BuildingInformation info = value as BuildingInformation;
+            if (info != null)
+            {
+                SpaceBoundaryCollection sbs = info.SpaceBoundaries as SpaceBoundaryCollection;
+                if (sbs != null) { return sbs.SecondLevelPhysicalInternalCount.ToString(); }
+            }
+            return "(no space boundaries loaded)";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new InvalidOperationException();
+        }
+    }
+
+    [ValueConversion(typeof(BuildingInformation), typeof(string))]
+    public class BuildingInformationTo2ndLevelPhysicalExternalCountString : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            BuildingInformation info = value as BuildingInformation;
+            if (info != null)
+            {
+                SpaceBoundaryCollection sbs = info.SpaceBoundaries as SpaceBoundaryCollection;
+                if (sbs != null) { return sbs.SecondLevelPhysicalExternalCount.ToString(); }
+            }
+            return "(no space boundaries loaded)";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new InvalidOperationException();
+        }
+    }
+
+    [ValueConversion(typeof(BuildingInformation), typeof(string))]
+    public class BuildingInformationTo3rdLevelCountString : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            BuildingInformation info = value as BuildingInformation;
+            if (info != null)
+            {
+                SpaceBoundaryCollection sbs = info.SpaceBoundaries as SpaceBoundaryCollection;
+                if (sbs != null) { return sbs.ThirdLevelCount.ToString(); }
+            }
+            return "(no space boundaries loaded)";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new InvalidOperationException();
+        }
+    }
+
+    [ValueConversion(typeof(BuildingInformation), typeof(string))]
+    public class BuildingInformationTo4thLevelCountString : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            BuildingInformation info = value as BuildingInformation;
+            if (info != null)
+            {
+                SpaceBoundaryCollection sbs = info.SpaceBoundaries as SpaceBoundaryCollection;
+                if (sbs != null) { return sbs.FourthLevelCount.ToString(); }
+            }
+            return "(no space boundaries loaded)";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new InvalidOperationException();
+        }
+    }
+
+    [ValueConversion(typeof(BuildingInformation), typeof(string))]
+    public class BuildingInformationToVirtualCountString : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            BuildingInformation info = value as BuildingInformation;
+            if (info != null)
+            {
+                SpaceBoundaryCollection sbs = info.SpaceBoundaries as SpaceBoundaryCollection;
+                if (sbs != null) { return sbs.VirtualCount.ToString(); }
+            }
+            return "(no space boundaries loaded)";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new InvalidOperationException();
+        }
+    }
 }
