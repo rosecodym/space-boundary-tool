79c767c64ee904f5281eadaebceb4da7b21906fc
cmrose <cmrose@lbl.gov>
2012-08-08 12:48:10 -0700

    beginnings of idd manager

---
+++ GUI/IddManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using LibIdf.Idd;
+
+namespace GUI
+{
+    class IddManager
+    {
+        private Dictionary<EnergyPlusVersion, string> filenames;
+    }
+}
