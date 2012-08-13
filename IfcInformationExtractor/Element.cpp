ad8b81c70c285e58eaab976df2344e6e1f15d3d0
cmrose <cmrose@lbl.gov>
2012-08-13 16:31:58 -0700

    when an ifc material can't be found, the dummy name now includes the source element guid

--- IfcInformationExtractor/Element.cpp
+++ IfcInformationExtractor/Element.cpp
@@ -9,70 +9,70 @@ namespace IfcInformationExtractor {
 
 namespace {
 
-Construction ^ createSingleMaterial(const cppw::Instance & inst) {
+Construction ^ createSingleMaterial(const cppw::Instance & inst, String ^ elementGuid) {
 	if (inst.is_kind_of("IfcMaterial")) {
 		return gcnew SingleMaterial(gcnew String(((cppw::String)inst.get("Name")).data()));
 	}
 	else if (inst.is_kind_of("IfcMaterialLayer")) {
 		cppw::Select mat = inst.get("Material");
-		return gcnew SingleMaterial (gcnew String(mat.is_set() ? 
-			((cppw::String)((cppw::Instance)mat).get("Name")).data() : "(unset material name for layer)"));
+		return gcnew SingleMaterial(mat.is_set() ?
+			gcnew String(((cppw::String)((cppw::Instance)mat).get("Name")).data()) : String::Format("(unset material name for layer - element {0})", elementGuid));
 	}
 	else {
-		return gcnew SingleMaterial(gcnew String("(material name for unknown source)"));
+		return gcnew SingleMaterial(String::Format("(material name for unknown source - element {0})", elementGuid));
 	}
 }
 
-Construction ^ createConstructionForLayerSet(const cppw::Instance & inst) {
+Construction ^ createConstructionForLayerSet(const cppw::Instance & inst, String ^ elementGuid) {
 	cppw::List mats = inst.get("MaterialLayers");
 	if (mats.count() > 1) {
 		cppw::Select name = inst.get("LayerSetName");
-		return gcnew CompositeConstruction(gcnew String(name.is_set() ? 
-			((cppw::String)name).data() : "(unnamed composite construction)"));
+		return gcnew CompositeConstruction(name.is_set() ? 
+			gcnew String(((cppw::String)name).data()) : String::Format("(unnamed composite construction - element {0})", elementGuid));
 	}
 	else {
-		return createSingleMaterial((cppw::Instance)mats.get_(0));
+		return createSingleMaterial((cppw::Instance)mats.get_(0), elementGuid);
 	}
 }
 
-Construction ^ createConstruction(const cppw::Instance & inst) {
+Construction ^ createConstruction(const cppw::Instance & inst, String ^ elementGuid) {
 	if (inst.is_kind_of("IfcMaterial") || inst.is_kind_of("IfcMaterialLayer")) {
-		return createSingleMaterial(inst);
+		return createSingleMaterial(inst, elementGuid);
 	}
 	else if (inst.is_kind_of("IfcMaterialLayerSet")) {
-		return createConstructionForLayerSet(inst);
+		return createConstructionForLayerSet(inst, elementGuid);
 	}
 	else if (inst.is_kind_of("IfcMaterialLayerSetUsage")) {
-		return createConstructionForLayerSet((cppw::Instance)inst.get("ForLayerSet"));
+		return createConstructionForLayerSet((cppw::Instance)inst.get("ForLayerSet"), elementGuid);
 	}
 	else if (inst.is_kind_of("IfcMaterialList")) {
-		return gcnew CompositeConstruction("(construction for material list)");
+		return gcnew CompositeConstruction(String::Format("(construction for material list - element {0})", elementGuid));
 	}
 	else {
-		return gcnew SingleMaterial("(material for unknown material source)");
+		return gcnew SingleMaterial(String::Format("(material for unknown material source - element {0})", elementGuid));
 	}
 }
 
-Construction ^ createConstructionForCommon(const cppw::Instance & element) {
+Construction ^ createConstructionForCommon(const cppw::Instance & element, String ^ elementGuid) {
 	cppw::Set relAssociates = element.get("HasAssociations");
 	for (relAssociates.move_first(); relAssociates.move_next(); ) {
 		cppw::Instance rel = relAssociates.get_();
 		if (rel.is_kind_of("IfcRelAssociatesMaterial")) {
-			return createConstruction((cppw::Instance)rel.get("RelatingMaterial"));
+			return createConstruction((cppw::Instance)rel.get("RelatingMaterial"), elementGuid);
 		}
 	}
-	return gcnew SingleMaterial("(material for missing material source)");
+	return gcnew SingleMaterial(String::Format("(material for missing material source - element {0})", elementGuid));
 }
 
-Construction ^ createConstructionFor(const cppw::Instance & buildingElement) {
-	return createConstructionForCommon(buildingElement);
+Construction ^ createConstructionFor(const cppw::Instance & buildingElement, String ^ elementGuid) {
+	return createConstructionForCommon(buildingElement, elementGuid);
 }
 
 } // namespace
 
-Element::Element(const cppw::Instance & inst)
-	: guid(gcnew String(((cppw::String)inst.get("GlobalId")).data())),
-	construction(createConstructionFor(inst))
-{ }
+Element::Element(const cppw::Instance & inst) : guid(gcnew String(((cppw::String)inst.get("GlobalId")).data()))
+{ 
+	construction = createConstructionFor(inst, guid);
+}
 
 } // namespace IfcInformationExtractor
