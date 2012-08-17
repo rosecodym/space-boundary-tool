b163218a30e973503f130dbe433481ce88dfff3c
cmrose <cmrose@lbl.gov>
2012-08-17 14:43:37 -0700

    idf generator loop cleaner now uses multiple passes

--- GUI/SbtExtensions.cs
+++ GUI/SbtExtensions.cs
@@ -9,24 +9,89 @@ namespace GUI
 {
     static class SbtExtensions
     {
+        private class Vector3
+        {
+            readonly private double x;
+            readonly private double y;
+            readonly private double z;
+
+            public Vector3(Point from, Point to)
+            {
+                x = to.X - from.X;
+                y = to.Y - from.Y;
+                z = to.Z - from.Z;
+            }
+
+            private Vector3(double x, double y, double z)
+            {
+                this.x = x;
+                this.x = y;
+                this.z = z;
+            }
+
+            public double Magnitude { get { return Math.Sqrt(x * x + y * y + z * z); } }
+
+            static public Vector3 CrossProduct(Vector3 u, Vector3 v)
+            {
+                return new Vector3(
+                    u.y * v.z - u.z * v.y,
+                    u.z * v.x - u.x * v.z,
+                    u.x * v.y - u.y * v.x);
+            }
+        }
+
         public static Polyloop Cleaned(this Polyloop loop, double eps)
         {
-            // only deals with coincidence right now
-            Func<Point, Point, bool> areEffectivelySame = (a, b) =>
+            if (loop.Vertices.Count < 3) { return new Polyloop(new Point[] { }); }
+
+            LinkedList<Point> pointList = new LinkedList<Point>(loop.Vertices);
+            LinkedListNode<Point>[] window = new LinkedListNode<Point>[3] { pointList.First, null, null };
+
+            Func<LinkedListNode<Point>[], bool> collapse = w =>
             {
-                return (a.X - b.X) * (a.X - b.X) + (a.Y - b.Y) * (a.Y - b.Y) + (a.Z - b.Z) * (a.Z - b.Z) < (eps * eps);
+                Vector3 bToC = new Vector3(w[1].Value, w[2].Value);
+                if (bToC.Magnitude < eps)
+                {
+                    pointList.Remove(w[2]);
+                    return true;
+                }
+                Vector3 aToC = new Vector3(w[0].Value, w[2].Value);
+                if (aToC.Magnitude < eps)
+                {
+                    pointList.Remove(w[1]);
+                    pointList.Remove(w[2]);
+                    return true;
+                }
+                Vector3 aToB = new Vector3(w[0].Value, w[1].Value);
+                if (aToB.Magnitude < eps || Vector3.CrossProduct(aToB, bToC).Magnitude < eps)
+                {
+                    pointList.Remove(w[1]);
+                    return true;
+                }
+                return false;
             };
 
-            LinkedList<Point> res = new LinkedList<Point>();
-            foreach (Point p in loop.Vertices)
+            Func<LinkedListNode<Point>, LinkedListNode<Point>> nextOf = n => n == pointList.Last ? pointList.First : n.Next;
+
+            int stepsSinceLastChange = 0;
+
+            while (pointList.Count >= 3 && stepsSinceLastChange < pointList.Count)
             {
-                if (res.Count == 0 || !areEffectivelySame(p, res.Last.Value)) { res.AddLast(p); }
+                window[1] = nextOf(window[0]);
+                window[2] = nextOf(window[1]);
+                if (!collapse(window))
+                {
+                    window[0] = nextOf(window[0]);
+                    ++stepsSinceLastChange;
+                }
+                else
+                {
+                    stepsSinceLastChange = 0;
+                }
             }
-            while (res.Count > 0 && areEffectivelySame(res.First.Value, res.Last.Value)) { res.RemoveLast(); }
 
-            if (res.Count < 3) { res.Clear(); }
-
-            return new Polyloop(res);
+            if (pointList.Count < 3) { return new Polyloop(new Point[] { }); }
+            else { return new Polyloop(pointList); }
         }
     }
 }
