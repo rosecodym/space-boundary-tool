b6515a690a708ae62236b5641a101e86a9bd39e2
cmrose <cmrose@lbl.gov>
2012-09-14 15:56:54 -0700

    if a space has no faces, the space is now skipped and a warning emitted

--- Core/src/load_spaces.h
+++ Core/src/load_spaces.h
@@ -10,7 +10,14 @@ std::vector<space> load_spaces(space_info ** infos, size_t space_count, equality
 	std::vector<space> res;
 	boost::transform(
 		boost::make_iterator_range(infos, infos + space_count) |
-			boost::adaptors::filtered([&filter](space_info * sp) { return filter(sp->id); }),
+			boost::adaptors::filtered([&filter](space_info * sp) -> bool { 
+				if (!sp->id) { return false; }
+				else if (sp->geometry.rep_type == REP_BREP && sp->geometry.rep.as_brep.face_count < 4) {
+					WARN_MSG("Warning - skipping space %s, as it only has %u face(s).\n", sp->id, sp->geometry.rep.as_brep.face_count);
+					return false;
+				}
+				else { return true; }
+			}),
 		std::back_inserter(res),
 		[c](space_info * s) -> space {
 			NOTIFY_MSG("Loading space %s...", s->id);
