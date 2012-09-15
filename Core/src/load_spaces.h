5e3ec16baf09e0b1ba334f2f1dcf3774bed06cb7
cmrose <cmrose@lbl.gov>
2012-09-15 13:10:51 -0700

    re-enabled the space filter, which had apparently gotten disabled somewhere

--- Core/src/load_spaces.h
+++ Core/src/load_spaces.h
@@ -11,7 +11,7 @@ std::vector<space> load_spaces(space_info ** infos, size_t space_count, equality
 	boost::transform(
 		boost::make_iterator_range(infos, infos + space_count) |
 			boost::adaptors::filtered([&filter](space_info * sp) -> bool { 
-				if (!sp->id) { return false; }
+				if (!sp->id || !filter(sp->id)) { return false; }
 				else if (sp->geometry.rep_type == REP_BREP && sp->geometry.rep.as_brep.face_count < 4) {
 					WARN_MSG("Warning - skipping space %s, as it only has %u face(s).\n", sp->id, sp->geometry.rep.as_brep.face_count);
 					return false;
