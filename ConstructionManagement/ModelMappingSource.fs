b4adee7fda97a709fcf4890665eee8ecc0f482be
cmrose <cmrose@lbl.gov>
2012-10-01 13:32:41 -0700

    implemented INotifyPropertyChanged for ModelMappingSource. thanks internet!

--- ConstructionManagement/ModelMappingSource.fs
+++ ConstructionManagement/ModelMappingSource.fs
@@ -1,6 +1,7 @@
 ﻿namespace ConstructionManagement.ModelConstructions
 
 open System
+open System.ComponentModel
 
 open ConstructionManagement.MaterialLibrary
 
@@ -9,11 +10,16 @@ type LibraryEntry = ConstructionManagement.MaterialLibrary.LibraryEntry
 type ModelMappingSource (name:string, forWindows:bool) =
     let mutable mappedTo = None
 
+    // this is part of the magic spell to implement INotifyPropertyChanged
+    let propertyChanged = Event<_, _>()
+
     member this.Name = name
     member this.IsForWindows = forWindows
     member this.MappingTarget
         with get () = if mappedTo.IsNone then Unchecked.defaultof<LibraryEntry> else mappedTo.Value
-        and set (value) = if value <> Unchecked.defaultof<LibraryEntry> then mappedTo <- Some(value) else mappedTo <- None
+        and set (value) = 
+            if value <> Unchecked.defaultof<LibraryEntry> then mappedTo <- Some(value) else mappedTo <- None
+            propertyChanged.Trigger(this, PropertyChangedEventArgs("MappingTarget"))
 
     override this.Equals(obj:Object) = 
         match obj with
@@ -38,3 +44,7 @@ type ModelMappingSource (name:string, forWindows:bool) =
             | null -> 1
             | :? ModelMappingSource as other -> (this :> IComparable<ModelMappingSource>).CompareTo(other)
             | _ -> raise (ArgumentException())
+
+    interface INotifyPropertyChanged with
+        [<CLIEvent>]
+        member this.PropertyChanged = propertyChanged.Publish
