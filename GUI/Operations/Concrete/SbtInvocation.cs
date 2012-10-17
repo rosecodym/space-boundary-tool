1d5719177cad7510543c15b37fe8cbd6c7e8a5db
cmrose <cmrose@lbl.gov>
2012-10-17 13:43:37 -0700

    the sb core now properly reports warnings

--- GUI/Operations/Concrete/SbtInvocation.cs
+++ GUI/Operations/Concrete/SbtInvocation.cs
@@ -43,7 +43,9 @@ namespace GUI.Operations
                 if (vm.SbElementFilter != null) { p.ElementGuidFilter = vm.SbElementFilter.Split(' '); }
                 if (vm.SbSpaceFilter != null) { p.SpaceGuidFilter = vm.SbSpaceFilter.Split(' '); }
                 p.Flags = (Sbt.EntryPoint.SbtFlags)Convert.ToInt32(vm.Flags, 16);
-                p.NotifyMessage = p.WarnMessage = p.ErrorMessage = msg => ReportProgress(msg);
+                p.NotifyMessage = msg => ReportProgress(msg);
+                p.WarnMessage = msg => ReportProgress(msg, ProgressEvent.ProgressEventType.Warning);
+                p.ErrorMessage = msg => ReportProgress(msg, ProgressEvent.ProgressEventType.Error);
                 return p;
             };
             PerformLongOperation = p =>
