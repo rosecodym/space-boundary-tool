6c2792a10ae1b8c9394f51b5f999b47da1af95f3
cmrose <cmrose@lbl.gov>
2012-10-23 18:25:17 -0700

    ConstructionManagement assembly description created
    ConstructionManagement/AssemblyInfo.fs now contains an assembly description.

--- ConstructionManagement/AssemblyInfo.fs
+++ ConstructionManagement/AssemblyInfo.fs
@@ -8,7 +8,7 @@ open System.Runtime.InteropServices
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [<assembly: AssemblyTitle("ConstructionManagement")>]
-[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyDescription("An SBT component to manage construction information in IFC and IDF files.")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("LBNL")>]
 [<assembly: AssemblyProduct("ConstructionManagement")>]
