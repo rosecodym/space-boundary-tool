542108c3a056a85d05696e2d9976e15f789b3a95
cmrose <cmrose@lbl.gov>
2012-10-23 17:34:54 -0700

    created initial version updating script
    A script has been added that walks specified source files and updates version numbers (in specified locations). This is to make it easier to update versions without potentially forgetting to change a number somewhere. Only a single file has been added to the script so far.

---
+++ Update Versions.fsx
@@ -0,0 +1,31 @@
+﻿open System.IO
+open System.Text.RegularExpressions
+
+let major = 1
+let minor = 4
+let revision = 4
+
+let shortString = sprintf "%i.%i.%i" major minor revision
+let fullString = sprintf "%i.%i.%i.0" major minor revision
+
+let solutionDir = @"C:/Users/Cody/Documents/Visual Studio 2010/Projects/space-boundary-tool"
+
+let buildNeedle prefix suffix =
+    sprintf @"(?<prefix>%s)\S+(?<suffix>%s)" (Regex.Escape(prefix)) (Regex.Escape(suffix))
+
+let operate (localDir, filename, prefix, suffix, useFull) =
+    let path = Path.Combine(solutionDir, localDir, filename)
+    let lines = File.ReadAllLines(path)
+    let needle = buildNeedle prefix suffix
+    let newVersion = if useFull then fullString else shortString
+    let replacement = sprintf @"${prefix}%s${suffix}" newVersion
+    use writer = new StreamWriter(path)
+    lines
+        |> Seq.map (fun line -> Regex.Replace(line, needle, replacement))
+        |> Seq.iter (fun line -> writer.WriteLine(line))
+
+let cases = [
+    ("Ifc Adapter/src", "add_to_model.cpp", "app.put(\"Version\", \"", "\");", false)
+    ]
+
+cases |> List.iter operate
