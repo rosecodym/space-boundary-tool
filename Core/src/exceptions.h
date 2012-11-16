53fe21c5b3b284a38c867ddab9b0ac44e3309f26
cmrose <cmrose@lbl.gov>
2012-11-15 18:03:37 -0800

    modified load_elements to correctly trap various bad geometry exceptions
    Previously, various bad input geometry would throw various exceptions, but load_elements didn't properly catch them so they reported false success. This shouldn't be happening anyway, but the exception structure has been modified slightly and load_elements itself changed to catch and report these exceptions.

--- Core/src/exceptions.h
+++ Core/src/exceptions.h
@@ -34,10 +34,26 @@ public:
 	virtual const char * condition() const throw() { return "b-rep with a face void"; }
 };
 
-class bad_brep_exception : public unsupported_geometry_exception {
+class bad_geometry_exception : public sbt_exception { 
+public:
+	virtual const char * what() const throw() {
+		std::string * str = new std::string(
+			(boost::format("Bad geometry (%s).") % condition()).str());
+		return str->c_str();
+	}
+	virtual const char * condition() const throw() = 0;
+};
+
+class bad_brep_exception : public bad_geometry_exception {
 public:
 	virtual const char * condition() const throw() { return "bad b-rep"; }
 };
 
+class invalid_face_exception : public bad_geometry_exception {
+public:
+	virtual const char * condition() const throw() { return "invalid face"; }
+};
+
+// This isn't a bad_geometry_exception because it indicates an inconsistent
+// program state, not bad geometry.
 class unknown_geometry_rep_exception : public sbt_exception { };
-class invalid_face_exception : public sbt_exception { };
--- Core/src/load_elements.cpp
+++ Core/src/load_elements.cpp
@@ -9,8 +9,15 @@
 
 using namespace reporting;
 
-std::vector<element> load_elements(element_info ** infos, size_t count, equality_context * c, const guid_filter & filter) {
-	report_progress(boost::format("Beginning loading for %u elements.\n") % count);
+std::vector<element> load_elements(
+	element_info ** infos, 
+	size_t count, 
+	equality_context * c, 
+	const guid_filter & filter) 
+{
+	typedef boost::format fmt;
+
+	report_progress(fmt("Beginning loading for %u elements.\n") % count);
 
 	std::vector<element> complex_elements;
 	for (size_t i = 0; i < count; ++i) {
@@ -20,15 +27,27 @@ std::vector<element> load_elements(element_info ** infos, size_t count, equality
 			}
 		}
 		catch (unsupported_geometry_exception & ex) {
-			report_warning(boost::format("Element %s has unsupported geometry (%s). It will be ignored.\n") % infos[i]->id % ex.condition());
+			report_warning(fmt(
+				"Element %s has unsupported geometry (%s). It will be "
+				"ignored.\n") 
+				% infos[i]->id % ex.condition());
 		}
 		catch (unknown_geometry_rep_exception & /*ex*/) {
-			report_warning(boost::format("Element %s has unknown internal geometry representation type. It will be ignored. Please report this SBT bug.\n") % infos[i]->id);
+			report_warning(fmt(
+				"Element %s has an unknown internal geometry representation "
+				"type. It will be ignored. Please report this SBT bug.\n") 
+				% infos[i]->id);
+		}
+		catch (bad_geometry_exception & ex) {
+			report_warning(fmt(
+				"Element %s has bad geometry (%s). It will be ignored.\n")
+				% infos[i]->id % ex.condition());
 		}
 	}
 
 	typedef std::vector<element>::iterator element_iterator;
-	typedef CGAL::Box_intersection_d::Box_with_handle_d<double, 3, element_iterator> element_box;
+	using CGAL::Box_intersection_d::Box_with_handle_d;
+	typedef Box_with_handle_d<double, 3, element_iterator> element_box;
 
 	std::vector<element_box> walls;
 	std::vector<element_box> slabs;
@@ -41,7 +60,9 @@ std::vector<element> load_elements(element_info ** infos, size_t count, equality
 		else if (e->type() == COLUMN) { columns.push_back(box); }
 	}
 
-	report_progress(boost::format("Got bounding boxes (%u walls, %u slabs, %u columns).\n") % walls.size() % slabs.size() % columns.size());
+	report_progress(fmt(
+		"Got bounding boxes (%u walls, %u slabs, %u columns).\n") 
+		% walls.size() % slabs.size() % columns.size());
 
 	bool need_wall_column_check = !walls.empty() && !columns.empty();
 	bool need_slab_column_check = !slabs.empty() && !columns.empty();
@@ -49,15 +70,16 @@ std::vector<element> load_elements(element_info ** infos, size_t count, equality
 	if (need_wall_column_check) {
 		for (auto w = walls.begin(); w != walls.end(); ++w) {
 
-			report_progress(boost::format("Resolving wall %s") % w->handle()->source_id().c_str());
+			report_progress(fmt(
+				"Resolving wall %s") % w->handle()->source_id().c_str());
 			bool performed_any_subtractions = false;
 
 			if (need_wall_column_check) {
-				boost::for_each(columns, [w, &performed_any_subtractions, c](const element_box & col) {
-					// The share_plane_opposite check will yield false negatives in
-					// extremely pathological cases (if two objects share an
-					// opposite face but intersect elsewhere) but I'm not worried
-					// about that.
+				boost::for_each(columns, [&, w, c](const element_box & col) {
+					// The share_plane_opposite check will yield false 
+					// negatives in extremely pathological cases (if two 
+					// objects share an opposite face but intersect elsewhere) 
+					// but I'm not worried about that.
 					if (CGAL::do_overlap(w->bbox(), col.bbox()) &&
 						!element::share_plane_opposite(
 							*w->handle(), 
@@ -71,17 +93,23 @@ std::vector<element> load_elements(element_info ** infos, size_t count, equality
 				});
 			}
 
-			report_progress(performed_any_subtractions ? "done.\n" : " - no resolution necessary.\n");
+			if (performed_any_subtractions) {
+				report_progress("done.\n");
+			}
+			else {
+				report_progress(" - no resolution necessary.\n");
+			}
 		}
 	}
 
 	if (need_slab_column_check) {
 		for (auto col = columns.begin(); col != columns.end(); ++col) {
 
-			report_progress(boost::format("Resolving column %s") % col->handle()->source_id().c_str());
+			report_progress(fmt(
+				"Resolving column %s") % col->handle()->source_id().c_str());
 			bool performed_any_subtractions = false;
 
-			boost::for_each(slabs, [col, &performed_any_subtractions, c](const element_box & s) {
+			boost::for_each(slabs, [&, col, c](const element_box & s) {
 				// The share_plane_opposite check will yield false negatives in
 				// extremely pathological cases (if two objects share an
 				// opposite face but intersect elsewhere) but I'm not worried
@@ -100,13 +128,21 @@ std::vector<element> load_elements(element_info ** infos, size_t count, equality
 				}
 			});
 
-			report_progress(performed_any_subtractions ? "done.\n" : " - no resolution necessary.\n");
+			if (performed_any_subtractions) {
+				report_progress("done.\n");
+			}
+			else {
+				report_progress(" - no resolution necessary.\n");
+			}
 		}
 	}
 
 	std::vector<element> res;
 	for (auto e = complex_elements.begin(); e != complex_elements.end(); ++e) {
-		element::explode_to_single_volumes(std::move(*e), c, std::back_inserter(res));
+		element::explode_to_single_volumes(
+			std::move(*e), 
+			c, 
+			std::back_inserter(res));
 	}
 
 	return res;
