672c1d736a69ddf2e381bc802ff2a2b037f4f2e3
cmrose <cmrose@lbl.gov>
2012-12-05 12:40:07 -0800

    modified conversion to interface structures to not write a billion dots
    Previously, the "conversion to interface structures" step wrote a separate period character for every space boundary. This looked stupid when there were hundreds of them. It now only writes 60 at most.

--- Core/src/convert_to_space_boundaries.h
+++ Core/src/convert_to_space_boundaries.h
@@ -20,24 +20,39 @@ space_boundary * create_unlinked_space_boundary(const surface & surf);
 
 template <typename SurfaceRange>
 sbt_return_t convert_to_space_boundaries(const SurfaceRange & surfaces, space_boundary *** sbs, size_t * sb_count) {
+	typedef std::unique_ptr<surface> surface_ptr;
+	typedef std::pair<std::string, space_boundary *> interfc_entry;
+
 	std::map<std::string, space_boundary *> boundaries;
 
+	int max_dots = 60;
+	int per_dot = surfaces.size() / max_dots;
+	if (per_dot == 0) { per_dot = 1; }
+	int curr_count = 0;
+	auto tick = [per_dot, &curr_count]() {
+		if (++curr_count % per_dot == 0) {
+			reporting::report_progress(".");
+		}
+	};
+
+	using namespace boost::adaptors;
+
 	try {
-		// create unlinked structures
-		boost::copy(surfaces
-			| boost::adaptors::transformed([](const std::unique_ptr<surface> & surf) -> std::pair<std::string, space_boundary *> { 
-				reporting::report_progress(".");
-				return std::make_pair(surf->guid(), impl::create_unlinked_space_boundary(*surf.get())); 
-			})
-			| boost::adaptors::filtered([](const std::pair<std::string, space_boundary *> & entry) { return entry.second != nullptr; }),
-			std::inserter(boundaries, boundaries.begin()));
+		std::vector<space_boundary *> unlinked;
+		for (auto s = surfaces.begin(); s != surfaces.end(); ++s) {
+			auto unlinked = impl::create_unlinked_space_boundary(*s->get());
+			if (unlinked != nullptr) {
+				boundaries[(*s)->guid()] = unlinked;
+			}
+			tick();
+		}
 
 		// link to opposites
 		boost::for_each(surfaces 
-			| boost::adaptors::filtered([&boundaries](const std::unique_ptr<surface> & surf) {
+			| filtered([&boundaries](const surface_ptr & surf) {
 				return surf->has_other_side() && boundaries.find(surf->guid()) != boundaries.end();
 			}),
-			[&boundaries](const std::unique_ptr<surface> & surf) {
+			[&boundaries](const surface_ptr & surf) {
 				auto matching_interface_structure = boundaries.find(surf->guid())->second;
 				if (matching_interface_structure->opposite == nullptr) {
 					auto matching_opposite = boundaries.find(surf->other_side()->guid());
@@ -51,10 +66,10 @@ sbt_return_t convert_to_space_boundaries(const SurfaceRange & surfaces, space_bo
 
 		// link to parents
 		boost::for_each(surfaces
-			| boost::adaptors::filtered([&boundaries](const std::unique_ptr<surface> & surf) {
+			| filtered([&boundaries](const surface_ptr & surf) {
 				return surf->parent() && boundaries.find(surf->guid()) != boundaries.end();
 			}),
-			[&boundaries](const std::unique_ptr<surface> & surf) {
+			[&boundaries](const surface_ptr & surf) {
 				auto matching_parent = boundaries.find(surf->parent()->guid());
 				if (matching_parent != boundaries.end()) {
 					boundaries[surf->guid()]->parent = matching_parent->second;
@@ -62,7 +77,7 @@ sbt_return_t convert_to_space_boundaries(const SurfaceRange & surfaces, space_bo
 			});
 
 		// set levels
-		boost::for_each(surfaces, [&boundaries](const std::unique_ptr<surface> & surf) {
+		boost::for_each(surfaces, [&boundaries](const surface_ptr & surf) {
 			auto matching_structure = boundaries.find(surf->guid());
 			if (matching_structure != boundaries.end()) {
 				matching_structure->second->level =
@@ -76,14 +91,14 @@ sbt_return_t convert_to_space_boundaries(const SurfaceRange & surfaces, space_bo
 
 		(*sbs) = (space_boundary **)malloc(sizeof(space_boundary *) * boundaries.size());
 		if (!*sbs) { throw failed_malloc_exception(); }
-		boost::copy(boost::adaptors::values(boundaries), *sbs);
+		boost::copy(values(boundaries), *sbs);
 		*sb_count = boundaries.size();
 		return SBT_OK;
 	}
 	catch (failed_malloc_exception &) {
 		reporting::report_error("An allocation failed while generating interface structures! Try simplifying the building to reduce the final space boundary count. SBT should be restarted.\n");
 		free(*sbs);
-		boost::for_each(boost::adaptors::values(boundaries), [](space_boundary * sb) { free(sb); });
+		boost::for_each(values(boundaries), [](space_boundary * sb) { free(sb); });
 		return SBT_FAILED_ALLOCATION;
 	}
 }
