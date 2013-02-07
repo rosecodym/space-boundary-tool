0142d78a9669a446b138fbcc3c65d5dee6142914
cmrose <cmrose@lbl.gov>
2013-02-06 17:08:50 -0800

    new opening assignment algorithm (for multi-space openings)
    Openings that span multiple spaces will now be assigned.

--- Core/src/assign_openings.h
+++ Core/src/assign_openings.h
@@ -19,8 +19,11 @@ void assign_openings(
 		reporting::report_progress("No openings to assign.\n");
 		return;
 	}
-	std::vector<std::unique_ptr<surface>> opening_surfaces;
+
+	typedef std::vector<std::unique_ptr<surface>> block_group;
+	std::vector<block_group> opening_surfaces;
 	reporting::report_progress("Assigning openings");
+
 	for (auto blk = openings.begin(); blk != openings.end(); ++blk) {
 		assert(blk->heights().second);
 		auto o = blk->block_orientation();
@@ -31,46 +34,54 @@ void assign_openings(
 		// blocks.
 		oriented_area gm_a(o, height_a, blk_area, !blk->sense());
 		oriented_area gm_b(o, height_b, blk_area, blk->sense());
+
 		std::vector<layer_information> layers;
 		const element & e = blk->material_layer().layer_element();
 		layers.push_back(layer_information(height_a, height_b, e));
-		std::unique_ptr<surface> surf_a;
-		std::unique_ptr<surface> surf_b;
+
+		opening_surfaces.push_back(block_group());
+
 		for (auto s = surfaces->begin(); s != surfaces->end(); ++s) {
-			if (!surf_a && (*s)->geometry().contains(gm_a, height_eps)) {
-				surf_a = std::unique_ptr<surface>(new surface(
-					std::move(gm_a),
+			auto intr = (*s)->geometry() * gm_a;
+			if (intr && !intr->area_2d().is_empty()) {
+				auto surf = std::unique_ptr<surface>(new surface(
+					*intr,
 					e,
 					(*s)->bounded_space(),
 					layers,
 					(*s)->is_external()));
-				surf_a->set_parent_maybe(s->get(), height_eps);
-				if (surf_a->is_external()) {
-					break;
-				}
+				surf->set_parent_maybe(s->get(), height_eps);
+				opening_surfaces.back().push_back(std::move(surf));
 			}
-			else if (!surf_b && (*s)->geometry().contains(gm_b, height_eps)) {
-				surf_b = std::unique_ptr<surface>(new surface(
-					std::move(gm_b),
+			intr = (*s)->geometry() * gm_b;
+			if (intr && !intr->area_2d().is_empty()) {
+				auto surf = std::unique_ptr<surface>(new surface(
+					*intr,
 					e,
 					(*s)->bounded_space(),
 					layers,
 					(*s)->is_external()));
-				surf_b->set_parent_maybe(s->get(), height_eps);
-				if (surf_b->is_external()) {
+				surf->set_parent_maybe(s->get(), height_eps);
+				opening_surfaces.back().push_back(std::move(surf));
+			}
+		}
+		reporting::report_progress(".");
+	}
+	for (auto g = opening_surfaces.begin(); g != opening_surfaces.end(); ++g) {
+		for (auto p = g->begin(); p != g->end(); ++p) {
+			for (auto q = p; q != g->end(); ++q) {
+				if (p != q &&
+					(*p)->geometry().sense() != (*q)->geometry().sense() &&
+					(*p)->geometry().area_2d() == (*q)->geometry().area_2d())
+				{
+					surface::set_other_sides(*p, *q);
 					break;
 				}
 			}
 		}
-		if (surf_a && surf_b) { surface::set_other_sides(surf_a, surf_b); }
-		if (surf_a || surf_b) { reporting::report_progress("."); }
-		if (surf_a) { opening_surfaces.push_back(std::move(surf_a)); }
-		if (surf_b) { opening_surfaces.push_back(std::move(surf_b)); }
+		std::move(g->begin(), g->end(), std::back_inserter(*surfaces));
 	}
-	std::move(
-		opening_surfaces.begin(), 
-		opening_surfaces.end(), 
-		std::back_inserter(*surfaces));
+
 	reporting::report_progress("done.\n");
 }
 
