8afc09e0367d060676af5917230e06a3cb7819d2
cmrose <cmrose@lbl.gov>
2013-03-11 16:38:09 -0700

    added some dummy creation functions to the test project
    These are useful when I need to create a space or element object but don't actually care about its geometry (which is actually pretty common).

--- Tests/src/common.h
+++ Tests/src/common.h
@@ -26,3 +26,20 @@ element_info * create_element(
 	element_id_t id, 
 	solid geometry);
 space_info * create_space(const char * name, solid geometry);
+
+inline element_info * create_dummy_element() {
+	return create_element("dummy element", WALL, 1,
+		create_ext(0, 0, 1, 1, create_face(4,
+			simple_point(0, 0, 0),
+			simple_point(0, 1, 0),
+			simple_point(1, 1, 0),
+			simple_point(1, 0, 0))));
+}
+
+inline space_info * create_dummy_space() {
+	return create_space("dummy space", create_ext(0, 0, 1, 1, create_face(4,
+		simple_point(0, 0, 0),
+		simple_point(0, 1, 0),
+		simple_point(1, 1, 0),
+		simple_point(1, 0, 0))));
+}
