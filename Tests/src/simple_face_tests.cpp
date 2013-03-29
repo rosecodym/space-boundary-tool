b0680553f1d3b4ae5959585ec2e5528444d7267b
cmrose <cmrose@lbl.gov>
2013-03-29 12:35:51 -0700

    added some simple_face tests
    I'm adding some fixup facilities to simple_faces so I need to make sure they still work correctly.

--- Tests/src/simple_face_tests.cpp
+++ Tests/src/simple_face_tests.cpp
@@ -9,7 +9,49 @@ namespace geometry_2d {
 
 namespace {
 
-TEST(SimpleFace, NonPlanarCorrected) {
+TEST(SimpleFaceXYTriangle, HasCorrectVertices) {
+	equality_context c(0.01);
+	simple_face f(create_face(3,
+		simple_point(0, 0, 0),
+		simple_point(1, 0, 0),
+		simple_point(0, 1, 0)), &c);
+
+	EXPECT_EQ(3, f.outer().size());
+	EXPECT_EQ(0, f.inners().size());
+
+	auto has_point = [&f](double x, double y, double z) {
+		return 
+			boost::find(f.outer(), point_3(x, y, z)) !=
+			f.outer().end();
+	};
+
+	EXPECT_TRUE(has_point(0, 0, 0));
+	EXPECT_TRUE(has_point(1, 0, 0));
+	EXPECT_TRUE(has_point(0, 1, 0));
+}
+
+TEST(SimpleFaceNonAxisTriangle, HasCorrectVertices) {
+	equality_context c(0.01);
+	simple_face f(create_face(3,
+		simple_point(1, 0, 0),
+		simple_point(0, 1, 0),
+		simple_point(0, 0, 1)), &c);
+
+	EXPECT_EQ(3, f.outer().size());
+	EXPECT_EQ(0, f.inners().size());
+
+	auto has_point = [&f](double x, double y, double z) {
+		return 
+			boost::find(f.outer(), point_3(x, y, z)) !=
+			f.outer().end();
+	};
+
+	EXPECT_TRUE(has_point(1, 0, 0));
+	EXPECT_TRUE(has_point(0, 1, 0));
+	EXPECT_TRUE(has_point(0, 0, 1));
+}
+
+TEST(SimpleFaceNonPlanar, EndsUpPlanar) {
 	equality_context c(0.01);
 	simple_face f(create_face(4, 
 		simple_point(-6603.14, 675.341, 242.094),
