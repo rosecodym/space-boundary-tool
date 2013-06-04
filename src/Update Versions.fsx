5832704a1067112ca8cd0b613c4094df5fb5f467
cmrose <cmrose@lbl.gov>
2013-06-04 16:07:48 -0700

    modified UpdateVersions.fsx to use the current directory instead of a hardcoded directory
    It now works by providing a function that performs the actual update (see the directions in the file for details).

--- src/Update Versions.fsx
+++ src/Update Versions.fsx
@@ -3,46 +3,50 @@
 // strings, so there's minimal risk of an improper change. However, this means
 // that each location must be manually specified (in the 'cases' variable).
 
+// To use:
+// 1. Set desired version number
+// 2. Send the entire file to the interactive F# console
+// 3. Run the defined updateVersions function with the solution directory as 
+//    its argument.
+
 open System.IO
 open System.Text.RegularExpressions
 
-let major = 1
-let minor = 5
-let revision = 6
+let updateVersions solutionDir =
+    let major = 1
+    let minor = 5
+    let revision = 6
 
-let shortString = sprintf "%i.%i.%i" major minor revision
-let fullString = sprintf "%i.%i.%i.0" major minor revision
+    let shortString = sprintf "%i.%i.%i" major minor revision
+    let fullString = sprintf "%i.%i.%i.0" major minor revision
 
-let solutionDir = 
-    @"C:/Users/Cody/Documents/Visual Studio 2010/Projects/space-boundary-tool"
+    let buildNeedle prefix suffix =
+        sprintf @"(?<prefix>%s)\S+(?<suffix>%s)" prefix suffix
 
-let buildNeedle prefix suffix =
-    sprintf @"(?<prefix>%s)\S+(?<suffix>%s)" prefix suffix
-
-let operate (localDir, filename, prefix, suffix, useFull) =
-    let path = Path.Combine(solutionDir, localDir, filename)
-    let lines = File.ReadAllLines(path)
-    let needle = buildNeedle prefix suffix
-    let newVersion = if useFull then fullString else shortString
-    let replacement = sprintf @"${prefix}%s${suffix}" newVersion
-    use writer = new StreamWriter(path)
-    lines
+    let operate (localDir, filename, prefix, suffix, useFull) =
+        let path = Path.Combine(solutionDir, localDir, filename)
+        let lines = File.ReadAllLines(path)
+        let needle = buildNeedle prefix suffix
+        let newVersion = if useFull then fullString else shortString
+        let replacement = sprintf @"${prefix}%s${suffix}" newVersion
+        use writer = new StreamWriter(path)
+        lines
         |> Seq.map (fun line -> Regex.Replace(line, needle, replacement))
-        |> Seq.iter (fun line -> writer.WriteLine(line))
+        |> Seq.iter writer.WriteLine
 
-let cases = [
-    ("ConstructionManagement", "AssemblyInfo.fs", 
-     "\[<assembly: Assembly(File)?Version\(\"", "\"\)>\]", true)
-    ("GUI", "MainWindow.xaml", "<Window\.Title>Space Boundary Tool ", 
-     "</Window\.Title>", false)
-    ("GUI/Properties", "AssemblyInfo.cs", 
-     "\[assembly: Assembly(File)?Version\(\"", "\"\)\]", true)
-    ("Ifc Adapter/src", "add_to_model.cpp", 
-     "app\.put\(\"Version\", \"", "\"\);", false)
-    ("EDM Managed Wrapper", "AssemblyInfo.cpp", 
-     "\[assembly:AssemblyVersionAttribute\(\"", "\"\)\];", true)
-    ("Core Managed Wrapper/Properties", "AssemblyInfo.cs", 
-     "\[assembly: Assembly(File)?Version\(\"", "\"\)\]", true)
-    ]
+    let cases = [
+        ("ConstructionManagement", "AssemblyInfo.fs", 
+         "\[<assembly: Assembly(File)?Version\(\"", "\"\)>\]", true)
+        ("GUI", "MainWindow.xaml", "<Window\.Title>Space Boundary Tool ", 
+         "</Window\.Title>", false)
+        ("GUI/Properties", "AssemblyInfo.cs", 
+         "\[assembly: Assembly(File)?Version\(\"", "\"\)\]", true)
+        ("Ifc Adapter/src", "add_to_model.cpp", 
+         "app\.put\(\"Version\", \"", "\"\);", false)
+        ("EDM Managed Wrapper", "AssemblyInfo.cpp", 
+         "\[assembly:AssemblyVersionAttribute\(\"", "\"\)\];", true)
+        ("Core Managed Wrapper/Properties", "AssemblyInfo.cs", 
+         "\[assembly: Assembly(File)?Version\(\"", "\"\)\]", true)
+        ]
 
-cases |> List.iter operate
+    cases |> List.iter operate
