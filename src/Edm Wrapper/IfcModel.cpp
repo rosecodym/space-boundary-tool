0dcd7da46f4634e5a39655a277d5ac2134935423
cmrose <cmrose@lbl.gov>
2013-06-26 15:54:33 -0700

    implemented basic site geography properties
    NorthAxis has never been implemented; I don't know why.

--- src/Edm Wrapper/IfcModel.cpp
+++ src/Edm Wrapper/IfcModel.cpp
@@ -22,4 +22,43 @@ IfcModel::IfcModel(String ^ path)
 	model_ = new cppw::Open_model(repo_->get_model(buf), cppw::RW_access);
 }
 
+double IfcModel::Elevation::get() {
+	double res = 0.0;
+	cppw::Instance site = model_->get_set_of("IfcSite").get_(0);
+	cppw::Select sel = site.get("RefElevation");
+	if (sel.is_set()) { res = (cppw::Real)sel; }
+	return res;
+}
+
+double IfcModel::NorthAxis::get() {
+	// I don't know why this isn't implemented but nobody's complained yet!
+	return 0.0;
+}
+
+double IfcModel::Latitude::get() {
+	double res = 0.0;
+	cppw::Instance site = model_->get_set_of("IfcSite").get_(0);
+	cppw::Select sel = site.get("RefLatitude");
+	if (sel.is_set()) {
+		cppw::List lst = sel;
+		res += (cppw::Real)lst.get_(0);
+		res += (cppw::Real)lst.get_(1) / 60.0;
+		res += (cppw::Real)lst.get_(2) / 360.0;
+	}
+	return res;
+}
+
+double IfcModel::Longitude::get() {
+	double res = 0.0;
+	cppw::Instance site = model_->get_set_of("IfcSite").get_(0);
+	cppw::Select sel = site.get("RefLongitude");
+	if (sel.is_set()) {
+		cppw::List lst = sel;
+		res += (cppw::Real)lst.get_(0);
+		res += (cppw::Real)lst.get_(1) / 60.0;
+		res += (cppw::Real)lst.get_(2) / 360.0;
+	}
+	return res;
+}
+
 } // namespace IfcInterface
