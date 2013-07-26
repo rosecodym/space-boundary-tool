4fa32112c0874fe0592cae9c430aac198a058903
cmrose <cmrose@lbl.gov>
2013-07-26 13:05:00 -0700

    removed the edm wrapper targets from the version update script
    Since that project gets built separately the versioning should be separate.

--- src/Update Versions.fsx
+++ src/Update Versions.fsx
@@ -43,8 +43,6 @@ let updateVersions solutionDir =
          "\[assembly: Assembly(File)?Version\(\"", "\"\)\]", true)
         ("Ifc Adapter/src", "add_to_model.cpp", 
          "version_string = \"", "\";", false)
-        ("Edm Wrapper", "AssemblyInfo.cpp", 
-         "\[assembly:AssemblyVersionAttribute\(\"", "\"\)\];", true)
         ("Core Managed Wrapper/Properties", "AssemblyInfo.cs", 
          "\[assembly: Assembly(File)?Version\(\"", "\"\)\]", true)
         ]
